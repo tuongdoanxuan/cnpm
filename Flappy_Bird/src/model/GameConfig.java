8c225fa9bf0194b65b7d7ad0253227334c0fec8b
haduydai <daithan348@gmail.com>
2026-06-01 22:32:05 +0700

    demo khởi tạo cấu hình dự án nạp tài nguyên hình ảnh âm thanh

---
+++ Flappy_Bird/src/model/GameConfig.java
@@ -0,0 +1,6 @@
+package model;
+
+public class GameConfig {
+	public static final int BOARD_WIDTH = 360+200;
+	public static final int BOARD_HEIGHT = 640;
+}
---
+++ Flappy_Bird/src/model/ImageAssets.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ImageAssets {
+	public static final Image backgroundImg = new ImageIcon("picture/flappybirdbg.png").getImage();
+	public static final Image birdImg = new ImageIcon("picture/flappybird.png").getImage();
+	public static final Image topPipeImg = new ImageIcon("picture/toppipe.png").getImage();
+	public static final Image bottomPipeImg = new ImageIcon("picture/bottompipe.png").getImage();
+	public static final Image HARD_PIPE = new ImageIcon("picture/pipe_hard.png").getImage();
+	public static final Image bulletImg = new ImageIcon("picture/bullet_v4.png").getImage();
+}
